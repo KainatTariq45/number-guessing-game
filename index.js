c6f9417aa9f5ffbb73b3a53b96a0fde5d48a7969
kainattariq <kainattariq122@example.com>
2024-04-02 15:03:10 +0500

    Added chalk and console

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 #! /usr/bin/env node  
 import inquirer from "inquirer";
+import chalk from "chalk";
 //1. computer generate random number    done 
 //2. user input for guessing number     done 
 //3. compare user input and computer generated number and show result 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(chalk.bgCyan.bold("\n WELCOME TO NUMBER GUESSING GAME"));
 const answer = await inquirer.prompt([
     { name: "userGuessedNumber",
         type: "number",
-        message: "please guess any number between 1-6"
+        message: chalk.green("Please guess any number between 1-6 ?")
     }
 ]);
 if (answer.userGuessedNumber === randomNumber) {
-    console.log("Congratulations you guessed right number");
+    console.log(chalk.blue("Congratulations you guessed right number"));
 }
 else {
-    console.log("oops! you guess wrong number");
+    console.log(chalk.magenta("oops! you guess wrong number"));
 }
